f5011a3c6fc8e76d058b323d3f0f769ab26bd335
Dudas Timea <h257330@stud.u-szeged.hu>
2024-11-26 19:53:09 +0100

    First phase (writing tests)

---
+++ homework/README.md
@@ -0,0 +1,9 @@
+# HOMEWORK
+
+### Project setup
+
+1.  Run the following command: 		`npm install`
+
+2. Run the project with: 	` npm start`
+
+3.  Run tests with : 	`npm test` 
---
+++ homework/tests/index.test.js
@@ -0,0 +1,30 @@
+const Greeter = require("../src/index");
+describe('Greeter', () => {
+    test('No input should greet with "my friend"', () => {
+        expect(Greeter.greet()).toBe('Hello, my friend.');
+    });
+
+    test('One name should include the name in the greeting', () => {
+        expect(Greeter.greet(['Bob'])).toBe('Hello Bob.');
+    });
+
+    test('Multiple names should list all names correctly', () => {
+        expect(Greeter.greet(['Alice', 'Bob', 'Jerry'])).toBe('Hello Alice, Bob and Jerry.');
+    });
+
+    test('All uppercase name should result in a shouting greeting', () => {
+        expect(Greeter.greet(['BARRY'])).toBe('HELLO BARRY!');
+    });
+
+    test('Mixed names should separate normal and shouting greetings', () => {
+        expect(Greeter.greet(['JAY', 'Maya', 'Alice', 'BOB', 'Charlotte']))
+            .toBe('Hello Maya, Alice and Charlotte. HELLO JAY AND BOB!');
+    });
+
+    test('Names containing commas should be split into separate names', () => {
+        expect(Greeter.greet(['Jerry', 'Alice, Bob']))
+            .toBe('Hello Jerry, Alice and Bob.');
+    });
+
+});
+
